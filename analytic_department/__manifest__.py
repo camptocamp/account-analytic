b536f26c2af549e5dc358ef9c234e4c8953eaec8
Tonow-c2c <thomas.nowicki@camptocamp.com>
2018-04-04 18:24:36 +0200

    [MIG] analytic_department: Migration to 11.0

--- analytic_department/__manifest__.py
+++ analytic_department/__manifest__.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-# © 2011-2016 Camptocamp SA
-# © 2013 Daniel Reis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Analytic Department Categorization",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Camptocamp, Daniel Reis,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
